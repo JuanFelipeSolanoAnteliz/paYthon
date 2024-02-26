009529da0bfcc5f5d169f52f86a86665825e9512
JuanFelipeSolanoAnteliz <160759957+JuanFelipeSolanoAnteliz@users.noreply.github.com>
2024-02-26 14:59:15 -0500

    factorial

--- ejercicioDos.py
+++ ejercicioDos.py
@@ -2,4 +2,4 @@ num = int(input("Ingrese el número del cual desea conocer el factorial: "))
 fact = 1
 for i in range(1, num+1):
     fact*=i
-    print("El factorial de ",num, "es: ", fact)
+    print("El factorial de ",num, "es: ", fact) 
