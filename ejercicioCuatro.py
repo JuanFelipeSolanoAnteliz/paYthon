5c8fd03d0d7951192cf39c8c5ee15c3230b244ae
JuanFelipeSolanoAnteliz <160759957+JuanFelipeSolanoAnteliz@users.noreply.github.com>
2024-02-26 15:59:23 -0500

    ejercicio conversion de temperaturas

---
+++ ejercicioCuatro.py
@@ -0,0 +1,3 @@
+grdCelsius = float(input("Ingrese la temperatura en °C que desea conocer en °F: "))
+grdFahrenheit = float(grdCelsius * 9/5 + 32 )
+float(print(f"""El valor en grados Fahrenheit es: {grdFahrenheit}"""))
