430dec56e4ce01f148e6ba65461a3a2fcff16673
JuanFelipeSolanoAnteliz <160759957+JuanFelipeSolanoAnteliz@users.noreply.github.com>
2024-02-26 16:50:58 -0500

    parOimpar

---
+++ ejercicioCinco.py
@@ -0,0 +1,13 @@
+num1 = int(input("Introduzca el un numero mayor que 0: "))
+
+if num1 == 0:
+    print("El nuúmero es 0 no es par ni impar")
+    
+elif num1 == 1:
+    print("El número es 1 no es par ni impar")
+    
+if num1 % 2 == 0:
+    print(num1,"es par")
+else:
+    print(num1,"es impar")
+    
