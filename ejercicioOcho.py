c3317fc9096199e4287345b4ea1198212c304356
JuanFelipeSolanoAnteliz <160759957+JuanFelipeSolanoAnteliz@users.noreply.github.com>
2024-02-29 18:40:14 -0500

    algoritmos

---
+++ ejercicioOcho.py
@@ -0,0 +1,9 @@
+lista = [9, 8, 34]
+valores = len(lista)
+suma = 0
+
+for numero in lista:
+    suma += numero
+
+promedio = suma / valores
+print(f'El promedio es: {promedio}')
---
+++ ejercicioSiete.py
@@ -0,0 +1,4 @@
+numero = int(input("Introduca el numero de la tabla que quiere ver: "))
+for Juan in range(0,11,1):
+    resultado = Juan * numero
+    print(numero, " X ", Juan ," = ",resultado)
