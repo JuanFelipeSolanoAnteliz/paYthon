de224e52fa9ac38bcb666e36109575a147f4f485
JuanFelipeSolanoAnteliz <160759957+JuanFelipeSolanoAnteliz@users.noreply.github.com>
2024-03-16 20:00:56 -0500

    quince

---
+++ ejerciciquince.py
@@ -0,0 +1,9 @@
+num1 = float(input("ingrese un numero: "))
+num2 = float(input("ingrese un numero: "))
+menorOmayor = sorted([num1, num2])
+
+print(f"Los numeros ordenados de menor a mayor son: {MenorMayor}")
+
+num1 = float(input("ingrese un numero: "))
+num2 = float(input("ingrese un numero: "))
+num3 = float(input("ingrese un numero: "))
