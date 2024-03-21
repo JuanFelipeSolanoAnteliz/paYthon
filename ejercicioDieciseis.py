a1bc87b9271fbdc4635622e4dd89f390b8467eb5
JuanFelipeSolanoAnteliz <160759957+JuanFelipeSolanoAnteliz@users.noreply.github.com>
2024-03-21 15:01:04 -0500

    terminar ejercicios

---
+++ ejercicioDieciseis.py
@@ -0,0 +1,9 @@
+#calculadora
+num1= float(input("ingrese el primer numero: "))
+num2= float(input("Ingres el segundo numero: "))
+operador = (input("ingrese un opreador ( +, -, * o /)"))
+
+if operador == "+" :
+    resultado = num1 + num2
+
+
